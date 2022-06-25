e085a05dc43c7dbf9be5f3f212788741375d9810
hotequil <hotequil@protonmail.com>
2022-06-25 16:05:33 -0300

    feat: done header and footer

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ import logo from './logo.png';
 import './App.css';
 
 function App() {
+  const name = "hotequil"
+
   return (
-    <header>
-      <img src={logo} alt="costgements logo" />
-    </header>
+    <div className="app">
+      <header className="app__header">
+        <img className="app__logo" src={logo} alt="costgements logo" />
+        <p className="app__title">costgements</p>
+      </header>
+      <main className="app__main">
+        <h1 className="text-align-center">Hello guys</h1>
+      </main>
+      <footer className="app__footer text-align-center">Developed by {name}</footer>
+    </div>
   );
 }
 
