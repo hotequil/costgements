a1a2ccec43707642fb70f8e55a053f296358c498
hotequil <hotequil@protonmail.com>
2022-06-26 17:59:04 -0300

    feat: done show link button validation

--- src/components/Partner.js
+++ src/components/Partner.js
@@ -2,16 +2,22 @@ import styles from "./Partner.module.css"
 import PropTypes from "prop-types";
 import { useState } from "react";
 import LinkButton from "./actions/LinkButton";
+import { generateMoreTextConfig, toggleMoreText } from "../helpers/more-text";
 
 function Partner({ title, description, since }){
   const alertTitle = () => alert(`Title: ${title}`);
   const [expanded, setExpanded] = useState(false);
+  const [descriptionConfig, setDescriptionConfig] = useState(generateMoreTextConfig());
+
+  toggleMoreText(descriptionConfig, setDescriptionConfig);
 
   return (
     <article className={styles.card}>
       <h2 className={styles.title}>{ title }</h2>
-      <p className={expanded ? styles.descriptionExpanded : styles.description}>{description}</p>
-      <LinkButton click={setExpanded.bind(this, !expanded)} text={expanded ? 'See less' : 'See more'} />
+      <p id={descriptionConfig.elementId} className={expanded ? styles.descriptionExpanded : styles.description}>{description}</p>
+
+      {descriptionConfig.show && <LinkButton click={setExpanded.bind(this, !expanded)} text={expanded ? 'See less' : 'See more'} />}
+
       <p className={styles.since}>Since { since }</p>
       <button className={styles.alert} onClick={alertTitle}>Show alert</button>
     </article>
