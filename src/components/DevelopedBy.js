b930a054cff8828968f75b9c844ed9a3a470c458
hotequil <hotequil@protonmail.com>
2022-06-26 14:34:57 -0300

    feat: done fragments

--- src/components/DevelopedBy.js
+++ src/components/DevelopedBy.js
@@ -2,7 +2,9 @@ import styles from "./DevelopedBy.module.css"
 
 function DevelopedBy({ name }){
   return (
-    <span className={styles.smallFont}>Developed by {name}</span>
+    <>
+      <span className={styles.smallFont}>Developed by {name}</span>&nbsp;<span className={styles.copy}>(copryright &copy;)</span>
+    </>
   )
 }
 
