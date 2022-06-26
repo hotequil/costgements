992686c8d6fa327cce242dd98fc06737e8840dab
hotequil <hotequil@protonmail.com>
2022-06-26 15:21:47 -0300

    feat: done new button and click

--- src/components/Partner.js
+++ src/components/Partner.js
@@ -2,11 +2,14 @@ import styles from "./Partner.module.css"
 import PropTypes from "prop-types";
 
 function Partner({ title, description, since }){
+  const alertTitle = () => alert(`Title: ${title}`);
+
   return (
     <article className={styles.card}>
       <h2 className={styles.title}>{ title }</h2>
       <p className={styles.description}>{ description }</p>
       <p className={styles.since}>Since { since }</p>
+      <button className={styles.alert} onClick={alertTitle}>Show alert</button>
     </article>
   )
 }
