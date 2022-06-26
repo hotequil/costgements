c3a85544f6737bf67f0cbfb0122a7ac0ee083ae5
hotequil <hotequil@protonmail.com>
2022-06-26 15:59:28 -0300

    feat: created expanded button with state

--- src/components/Partner.js
+++ src/components/Partner.js
@@ -1,13 +1,16 @@
 import styles from "./Partner.module.css"
 import PropTypes from "prop-types";
+import { useState } from "react";
 
 function Partner({ title, description, since }){
   const alertTitle = () => alert(`Title: ${title}`);
+  const [expanded, setExpanded] = useState(false);
 
   return (
     <article className={styles.card}>
       <h2 className={styles.title}>{ title }</h2>
-      <p className={styles.description}>{ description }</p>
+      <p className={expanded ? styles.descriptionExpanded : styles.description}>{description}</p>
+      <button onClick={setExpanded.bind(this, !expanded)} className="link">{expanded ? 'See less' : 'See more'}</button>
       <p className={styles.since}>Since { since }</p>
       <button className={styles.alert} onClick={alertTitle}>Show alert</button>
     </article>
