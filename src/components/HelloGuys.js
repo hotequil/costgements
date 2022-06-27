8b2acac3dced09b3c7898c9848dbcba874de045e
hotequil <hotequil@protonmail.com>
2022-06-27 20:52:51 -0300

    feat: removed center-abosolute class

--- src/components/HelloGuys.js
+++ src/components/HelloGuys.js
@@ -1,6 +1,6 @@
 function HelloGuys(){
   return (
-    <h1 className="no-margin text-align-center center-absolute font-home-title">Hello guys, welcome to the costgements!</h1>
+    <h1 className="no-margin text-align-center font-home-title">Hello guys, welcome to the costgements!</h1>
   )
 }
 
