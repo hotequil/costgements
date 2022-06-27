9d7301e1f3b274e1b9c962417ad9d0bd64de453c
hotequil <hotequil@protonmail.com>
2022-06-27 19:42:32 -0300

    feat: created list component for partner

---
+++ src/components/lists/PartnerList.js
@@ -0,0 +1,33 @@
+import styles from "./PartnerList.module.css"
+import PropTypes from "prop-types";
+import Partner from "../Partner";
+import NotFound from "../messages/NotFound";
+
+function PartnerList({ partners }) {
+  return (
+    <section className={styles.container}>
+      <h2 className={styles.title}>Partners</h2>
+
+      {
+        !partners.length ? <NotFound message="There are no partners" /> :
+        <ul className={styles.list}>
+          {
+            partners.map(({ name, description, since }, index) =>
+              <li key={index}><Partner title={name} description={description} since={since} /></li>
+            )
+          }
+        </ul>
+      }
+    </section>
+  );
+}
+
+PartnerList.propTypes = {
+  partners: PropTypes.array.isRequired
+}
+
+PartnerList.defaultProps = {
+  partners: []
+}
+
+export default PartnerList
