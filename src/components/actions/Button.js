32d13c093ec739d2d86ec8724d61cef203810782
hotequil <hotequil@protonmail.com>
2022-06-29 21:02:05 -0300

    feat: created button outline style

--- src/components/actions/Button.js
+++ src/components/actions/Button.js
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import { isNullOrUndefined } from "../../helpers/validations";
 import styles from "./Button.module.css"
+import PropTypes from "prop-types";
 
 export const layoutTypes = {
   BUTTON: "button",
@@ -12,24 +13,35 @@ export const buttonTypes = {
   SUBMIT: "submit",
 }
 
-export function Button({ children, click, to, layoutType, buttonType }){
+export function Button({ click, to, isOutline, layoutType, buttonType, children }){
   if(layoutType !== layoutTypes.BUTTON && layoutType !== layoutTypes.LINK) layoutType = layoutTypes.BUTTON
 
   const isLayoutTypeButton = layoutType === layoutTypes.BUTTON
   const isLayoutTypeLink = layoutType === layoutTypes.LINK
+  let classes = styles.button;
 
-  if(isLayoutTypeButton && isNullOrUndefined(click)) console.error('set click, please')
+  if(isLayoutTypeButton && isNullOrUndefined(click) && buttonType === buttonTypes.BUTTON) console.error('set click, please')
   if(isLayoutTypeLink && isNullOrUndefined(to)) console.error('set to, please')
+  if(isOutline) classes = `${classes} ${styles.buttonOutline}`;
 
   return (
     <>
-      { isLayoutTypeButton && <button className={styles.button} type={buttonType} onClick={click}>{children}</button> }
-      { isLayoutTypeLink && <Link className={styles.button} to={to}>{children}</Link> }
+      { isLayoutTypeButton && <button className={classes} type={buttonType} onClick={click}>{children}</button> }
+      { isLayoutTypeLink && <Link className={classes} to={to}>{children}</Link> }
     </>
   )
+}
+
+Button.propTypes = {
+  click: PropTypes.func,
+  to: PropTypes.string,
+  layoutType: PropTypes.string,
+  buttonType: PropTypes.string,
+  isOutline: PropTypes.bool,
 }
 
 Button.defaultProps = {
   layoutType: layoutTypes.BUTTON,
   buttonType: buttonTypes.BUTTON,
+  isOutline: false,
 }
