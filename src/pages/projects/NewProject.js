e394a3fe36a9d145f5d37b0b11a504edcc4d9843
hotequil <hotequil@protonmail.com>
2022-06-29 21:03:11 -0300

    feat: added actions in new project

--- src/pages/projects/NewProject.js
+++ src/pages/projects/NewProject.js
@@ -1,17 +1,21 @@
 import styles from "./NewProject.module.css"
 import { Input } from "../../components/forms/Input";
 import { useState } from "react";
-import { Button, layoutTypes } from "../../components/actions/Button";
+import { Button, buttonTypes, layoutTypes } from "../../components/actions/Button";
 
 export const NewProject = () => {
   const [, setName] = useState(null);
+  const submit = event => event.preventDefault()
 
   return (
-    <form className={`form ${styles.form}`}>
+    <form className={`form ${styles.form}`} onSubmit={submit}>
       <fieldset className="form__fieldset">
         <legend className="form__legend">New project</legend>
         <Input name="name" set={setName} label="Name" placeholder="Set name" required={true} />
-        <Button layoutType={layoutTypes.LINK} to="/projects">Back</Button>
+        <div className="form__actions">
+          <Button isOutline={true} layoutType={layoutTypes.LINK} to="/projects">Back</Button>
+          <Button layoutType={layoutTypes.BUTTON} buttonType={buttonTypes.SUBMIT}>Create</Button>
+        </div>
       </fieldset>
     </form>
   )
