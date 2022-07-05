35eb7000a38d1935630fe39bd280a9db44cc0369
hotequil <hotequil@protonmail.com>
2022-07-04 22:04:49 -0300

    feat: done ServicesList

--- src/pages/projects/services/ServicesList.js
+++ src/pages/projects/services/ServicesList.js
@@ -1,11 +1,13 @@
 import PropTypes from "prop-types";
 import { NewService } from "./NewService";
 import { generateToast, Toast, toastType } from "../../../components/global/toast/Toast";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Projects } from "../../../services/projects";
 import { uniqueId } from "../../../helpers/unique-id";
 import styles from "./ServicesList.module.css";
 import { copy } from "../../../helpers/manipulate";
+import NotFound from "../../../components/messages/NotFound";
+import { ServiceCard } from "./ServiceCard";
 
 const projectService = new Projects()
 
@@ -14,6 +16,13 @@ export const ServicesList = ({ project, setProject }) => {
   const [toast, setToast] = useState(generateToast())
   const [name, setName] = useState("")
   const [saving, setSaving] = useState(false)
+  const [services, setServices] = useState([])
+  const [removeList, setRemoveList] = useState([])
+
+  const setProjects = project => {
+    setProjectCopy(project)
+    setProject(project)
+  }
 
   const submit = async event => {
     event.preventDefault()
@@ -24,8 +33,7 @@ export const ServicesList = ({ project, setProject }) => {
       const services = [...projectCopy.services, { id: uniqueId(), name }]
       const response = await projectService.update({ ...projectCopy, services })
 
-      setProjectCopy(response)
-      setProject(response)
+      setProjects(response)
       setName("")
       setToast(generateToast("Created new service", toastType.SUCCESS))
     } catch(error){
@@ -37,11 +45,48 @@ export const ServicesList = ({ project, setProject }) => {
     }
   }
 
+  const remove = async (id, name) => {
+    setRemoveList([...removeList, id])
+
+    try{
+      const response = await projectService.update({
+        ...projectCopy,
+        services: projectCopy.services.filter(service => service.id !== id)
+      })
+
+      setProjects(response)
+      setToast(generateToast(`Removed ${name} service`, toastType.SUCCESS))
+    } catch(error){
+      console.error(error)
+
+      setToast(generateToast(`Error on remove ${name} service`, toastType.ERROR))
+    } finally {
+      setRemoveList(removeList.filter(itemId => itemId !== id))
+    }
+  }
+
+  useEffect(() => {
+    setServices(projectCopy.services)
+  }, [projectCopy])
+
   return (
     <div className={styles.container}>
       <Toast {...toast} set={setToast} />
       <NewService submit={submit} name={name} setName={setName} saving={saving} project={projectCopy} />
-      <div>TODO SERVICES LIST</div>
+      <h3 className={styles.title}>Services</h3>
+      <ul className={services.length ? styles.list : `${styles.list} ${styles.listNotFound}`}>
+        {
+          !services.length ?
+          <NotFound message="There are no services" /> :
+          services.map(({ id, name }) =>
+            <li key={id}>
+              <ServiceCard onRemove={remove.bind(null, id, name)}
+                           removing={removeList.includes(id)}
+                           name={name} />
+            </li>
+          )
+        }
+      </ul>
     </div>
   )
 }
